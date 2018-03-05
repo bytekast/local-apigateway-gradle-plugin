6efc54a1812f146a9c9fc76580ae415ee0aa5aba
Rowell Belen <developer@bytekast.com>
2018-03-04 21:59:11 -0700

    Work In Progress

--- src/main/resources/ratpack.groovy
+++ src/main/resources/ratpack.groovy
@@ -1,3 +1,4 @@
+import groovy.json.JsonOutput
 import org.yaml.snakeyaml.Yaml
 import ratpack.handling.Context
 
@@ -7,26 +8,50 @@ final serverlessYml = new File("${System.getProperty('project.rootDir')}/serverl
 final yaml = new Yaml().load(serverlessYml)
 final functions = yaml.functions?.keySet()?.collect { name -> yaml.functions.get(name) }
 
-def functionHandlers = []
+final functionHandlers = []
 functions?.each { f ->
-  def clazz = f.handler?.tokenize('::')?.first()
-  def instance = Class.forName(clazz).newInstance()
+  final clazz = f.handler?.tokenize('::')?.first()
+  final instance = Class.forName(clazz).newInstance()
 
-  def method = f.handler?.tokenize('::')?.last()
-  def handler = instance.&"${method}"
+  final method = f.handler?.tokenize('::')?.last()
+  final handler = instance.&"${method}"
 
-  def httpEvents = f?.events?.http
-
+  final httpEvents = f?.events?.http
   httpEvents?.each {
     def path = it.path?.toString()?.replaceAll('\\{', ":").replaceAll("}", "")
     functionHandlers << [
         method : it.method,
         path   : path,
         handler: { Context context ->
-          def input = context.request.path
-          println context.pathTokens instanceof Map
-          println context.request.queryParams
-          context.render handler(input, null)
+
+          final lambdaContext = new Expando()
+          lambdaContext.with {
+            getAwsRequestId = { UUID.randomUUID().toString() }
+            getFunctionName = { 'local' }
+            getRemainingTimeInMillis = { 20000 }
+            getInvokedFunctionArn = { UUID.randomUUID().toString() }
+          }
+
+          context.request.body.then({ data ->
+            final input = [
+                httpMethod           : context.request.method.toString(),
+                queryStringParameters: context.request.queryParams,
+                pathParameters       : context.pathTokens,
+                resource             : context.request.path,
+                headers              : context.request.headers.asMultiValueMap(),
+                body                 : data.getText()
+            ]
+
+            final result = handler(input, lambdaContext as com.amazonaws.services.lambda.runtime.Context)
+            if (result instanceof String) {
+              context.response.send(result)
+            } else if (result instanceof Map) {
+              final body = result.body ?: result
+              context.response.send(JsonOutput.toJson(body))
+            } else {
+              context.response.send(JsonOutput.toJson(result))
+            }
+          })
         }
     ]
   }
@@ -39,8 +64,3 @@ ratpack {
     }
   }
 }
-
-
-
-
-
